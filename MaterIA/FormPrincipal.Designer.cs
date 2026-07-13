a132c3e5283ad539f0081bff4eee0f12c8486324
Marcial <marcialtarauneclaure975@gmail.com>
2026-07-12 23:14:13 -0400

    Agregando fondo en metodo conteo

--- MaterIA/FormPrincipal.Designer.cs
+++ MaterIA/FormPrincipal.Designer.cs
@@ -141,6 +141,7 @@
             pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox4.TabIndex = 7;
             pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
             // 
             // pictureBox5
             // 
